0f5f569ab702def73ce01203460536dd6ce33044
Yuichiro Ikeda <yibk18@gmail.com>
2019-12-04 09:46:10 +0900

    fixed basic_authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+    protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up,keys:[:fullname])
-    devise_parameter_sanitizer.permit(:account_update,keys: [:fullname])
-  end
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up,keys:[:fullname])
+        devise_parameter_sanitizer.permit(:account_update,keys: [:fullname])
+    end
 
 end
