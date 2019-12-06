ffce83eb86bf44ea7ed575351de4d02032c7d7e0
Yuichiro Ikeda <yibk18@gmail.com>
2019-12-06 11:14:58 +0900

    created facebook authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_03_042930) do
+ActiveRecord::Schema.define(version: 2019_12_05_041703) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,6 +25,9 @@ ActiveRecord::Schema.define(version: 2019_12_03_042930) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "fullname"
+    t.string "provider"
+    t.string "uid"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
