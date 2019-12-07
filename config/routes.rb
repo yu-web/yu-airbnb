e7567f8717bfb04589257b92109abb97ce1e9f15
Yuichiro Ikeda <yibk18@gmail.com>
2019-12-07 18:26:54 +0900

    fixed indension

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   path_names: {sign_in: 'login',sign_up: 'registration',sign_out: 'logout'},
   controllers: { omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
