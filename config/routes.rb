12dc5b4262075990915437021f4c6512985f1745
yu-web <54567280+yu-web@users.noreply.github.com>
2019-12-07 20:40:21 +0900

    created edit profile (#8)
    * created edit profile
    
    * fixed indension

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "pages#home"
   devise_for :users,path:'',
   path_names: {sign_in: 'login',sign_up: 'registration',sign_out: 'logout'},
-  controllers: { omniauth_callbacks: 'omniauth_callbacks' }
+  controllers: { omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
