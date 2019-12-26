0c448fbf3b45fafad329493fd0526c3ccfb0f526
Yuichiro Ikeda <yibk18@gmail.com>
2019-12-26 10:29:05 +0900

    fixed basic search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -23,13 +23,13 @@ class PagesController < ApplicationController
     @arrayRooms = @rooms.to_a
 
     #step4 -for checking date availability
-   if(!params[:start_date].empty? && !params[:start_date].empty?)
-    @start_date = Date.parse(params[:strat_date])
+   if(!params[:start_date].empty? && !params[:end_date].empty?)
+    @start_date = Date.parse(params[:start_date])
     @end_date = Date.parse(params[:end_date])
     @rooms.each do |room|
-        taken_room = room.reservation.where("(? <= start_date AND start_date <= ?) OR(? <= end_date AND end_date <= ?)"),
+        taken_room = room.reservations.where("(? <= start_date AND start_date <= ?) OR (? <= end_date AND end_date <= ?)",
         @start_date,@end_date,
-        @start_date,@end_date.limit(1)
+        @start_date,@end_date).limit(1)
 
         if taken_room.any?
           @arrayRooms.delete(room)
